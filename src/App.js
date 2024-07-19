055417a00e52116f6639d1429ee104a83a1da268
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-19 14:28:52 +0530

    Showing Error Pages with errorElement

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Product";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
